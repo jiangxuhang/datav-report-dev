030948ee0b89b35e9e9c83e3f99bda14981a55d4
jiangxuhang <20157008@s.hlju.edu.cn>
2020-06-13 16:50:50 +0800

    顶部组件

---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <common-card title="累计销售额" value="￥32141352462">
+    <template>
+      <div class="compare">
+
+      </div>
+    </template>
+    <template #footer>
+      <span>昨日销售额</span>
+      <span class="money">￥3000000</span>
+    </template>
+  </common-card>
+</template>
+<script>
+import commonCardMixin from '../../mixins/commonCardMixin'
+export default {
+  mixins: [commonCardMixin]
+}
+</script>
+<style lang="scss" scoped>
+span {
+  font-size: 12px;
+}
+.compare {
+  height: 100%;
+  background: yellow;
+}
+.money {
+  margin-left: 5px;
+  color: #333;
+  font-weight: 700;
+}
+</style>
